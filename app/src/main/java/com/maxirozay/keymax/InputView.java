21901dbba72cd7ebfe1ca85f91c993a18347d190
Maxime Rossier <rossier.maxime@gmail.com>
2017-08-18 14:32:54 +0200

    fix duplicate recent emoji

--- app/src/main/java/com/maxirozay/keymax/InputView.java
+++ app/src/main/java/com/maxirozay/keymax/InputView.java
@@ -167,7 +167,7 @@ public class InputView extends LinearLayout {
     protected void addEmoji(String emoji) {
         int index = recentEmojis.indexOf(emoji);
         if (index > 0) recentEmojis.remove(index);
-        recentEmojis.add(0, emoji);
+        if (index != 0) recentEmojis.add(0, emoji);
     }
 
     protected void updateRecentEmojis() {
